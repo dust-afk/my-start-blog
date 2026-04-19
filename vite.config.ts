6db88b2a6f98b84a9210198944ffb5574f30bd93
squrrielsweetie <dust4567@qq.com>
2026-04-19 09:37:00 +0800

    chore: update vercel config for static site

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+// import { fileURLToPath, URL } from 'node:url'
+// import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+{
+  "framework": "vite",
+  "buildCommand": "npm run build",
+  "outputDirectory": "dist",
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "/index.html"
     }
-  }
-})
+  ]
+}
