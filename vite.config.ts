a1b2723a388ef989e4887d8edef5a16c9ba0cb08
squrrielsweetie <dust4567@qq.com>
2026-04-19 09:47:52 +0800

    fix: syntax error in vite.config.ts (missing comma)

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
-// import { fileURLToPath, URL } from 'node:url'
-// import vue from '@vitejs/plugin-vue'
-// import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
-// export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
-{
-  "framework": "vite",
-  "buildCommand": "npm run build",
-  "outputDirectory": "dist",
-  "routes": [
-    {
-      "src": "/(.*)",
-      "dest": "/index.html"
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  ]
-}
+  }
+})
